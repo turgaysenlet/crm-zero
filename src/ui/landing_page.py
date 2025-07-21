18abd6ab5c862acb8c6aa9e032e44ced0128d26d
Turgay <turgaysenlet@gmail.com>
2025-07-21 15:50:05 -0500

    Update landing page layout to be centered. Change welcome image to be Ghibli style!

--- src/ui/landing_page.py
+++ src/ui/landing_page.py
@@ -7,12 +7,15 @@ app.add_static_files("/resources", os.path.join(os.path.dirname(__file__), "../.
 
 @ui.page("/")
 def landing_page():
-    ui.label("Welcome to CRM Zero").classes("text-xl font-italic h2")
     ui.run_javascript('document.title = "CRM Zero"')
+    with ui.row().classes("w-full items-center justify-center gap-2 mb-4"):
+        ui.label("Welcome to CRM Zero").classes("text-xl font-italic h2")
 
-    ui.image("/resources/crm-zero1.png").style("height: 600px; width: 600px;")
+    with ui.row().classes("w-full items-center justify-center gap-2 mb-4"):
+        ui.image("/resources/crm-zero1.png").style("height: 600px; width: 600px;")
 
-    ui.link("API Console", "/docs").classes("text-xl font-bold")
-    ui.link("Create Case", "/ui/create_case").classes("text-xl font-bold")
-    ui.link("Create Case Comment", "/ui/create_case_comment").classes("text-xl font-bold")
-    ui.link("Workflow Step Editor", "/ui/workflow_step_editor").classes("text-xl font-bold")
+    with ui.row().classes("w-full items-center justify-center gap-2 mb-4"):
+        ui.link("API Console", "/docs").classes("text-xl font-bold")
+        ui.link("Create Case", "/ui/create_case").classes("text-xl font-bold")
+        ui.link("Create Case Comment", "/ui/create_case_comment").classes("text-xl font-bold")
+        ui.link("Workflow Step Editor", "/ui/workflow_step_editor").classes("text-xl font-bold")
